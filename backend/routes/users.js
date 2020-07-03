000c1aec375e5503f5b705ac3c836fab9cc0860f
Mark W <markwuu@gmail.com>
2020-07-03 16:08:27 -0700

    Saved hashed password to database

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 let User = require('../models/users.model');
 
 router.route('/').get((req, res) => {
@@ -8,17 +9,19 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = req.body.password;
+router.route('/add').post( async (req, res) => {
+  const { username, email, password } = req.body;
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log('hashedpassword', hashedPassword);
   const newUser = new User({
     _id: new mongoose.Types.ObjectId(),
     username,
     email,
-    password
+    password: hashedPassword
   });
+
+  console.log('newuser', newUser);
 
   newUser.save()
     .then(() => res.json('User added!'))
