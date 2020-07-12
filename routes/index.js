18b0a471df3b9388f47ebf6059a0eac27be8c98f
Mark W <markwuu@gmail.com>
2020-07-11 21:36:13 -0700

    Displayed points for unfinihsed goals on dashboard page

--- routes/index.js
+++ routes/index.js
@@ -7,26 +7,33 @@ const { unsubscribe } = require('./goals');
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
-// Dashboard
+// Dashboard Page
 router.get('/dashboard', ensureAuthenticated, async (req, res) => {
 
   const userGoals = await User.findById(req.user._id).populate('goals');
+  console.log('usergoals', userGoals);
 
   let uncompletedGoals = [];
   let completedGoals = [];
+  let score = [];
 
   for(let i = 0; i < userGoals.goals.length; i++){
+    console.log(userGoals.goals[i].points, userGoals.goals[i].success);
     if(!userGoals.goals[i].success !== true){
       completedGoals.push(userGoals.goals[i].description);
     } else {
+      score.push(userGoals.goals[i].points)
       uncompletedGoals.push(userGoals.goals[i].description)
     }
   }
 
+  console.log('score', score);
+
   res.render('dashboard', {
     user: req.user,
     completedGoals,
-    uncompletedGoals
+    uncompletedGoals,
+    score
   });
 
   goalsArray = [];
